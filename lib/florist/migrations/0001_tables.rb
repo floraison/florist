d17e82b36231cb5ade047c952f9e7ec222c6b1d8
John Mettraux <jmettraux@gmail.com>
2019-02-13 18:31:44 +0900

    Bring in flor-worklist migration

---
+++ lib/florist/migrations/0001_tables.rb
@@ -0,0 +1,68 @@
+
+Sequel.migration do
+
+  up do
+
+    create_table :flor_tasks do
+
+      primary_key :id, type: :Integer
+      String :domain, null: false
+      String :exid, null: false
+      String :nid, null: false
+
+      File :content # JSON
+        # the task payload and more...
+
+      String :ctime, null: false  # creation time
+      String :mtime, null: false  # last modification time
+      #String :atime, null: false  # archival time
+
+      String :status, null: false
+        # http://www.workflowpatterns.com/patterns/resource/
+        # "created"
+        # "offered"
+        # "allocated"
+        # "started"
+        # "suspended"
+        # "failed"
+        # "completed"
+
+      index :domain
+      index :exid
+      index [ :exid, :nid ]
+    end
+
+    create_table :flor_task_assignments do
+
+      primary_key :id, type: :Integer
+
+      Integer :flor_task_id, null: false
+      #foreign_key :flor_task_id, :flor_tasks, on_delete: :cascade
+
+      String :type, null: false  # "", "forced", "automatic", "escalated", ...
+
+      String :resource_name, null: false  # "bob", "accounting"
+      String :resource_type, null: false  # "user", "group", ...
+
+      File :content # JSON
+        # some metadata for this taskee/task pair
+
+      String :ctime, null: false  # creation time
+      String :mtime, null: false  # last modification time
+      #String :atime, null: false  # archival time
+
+      String :status, null: false
+        # "active" / "archived"
+
+      index :flor_task_id
+      index :resource_name
+    end
+  end
+
+  down do
+
+    drop_table :flor_tasks
+    drop_table :flor_task_assignments
+  end
+end
+
