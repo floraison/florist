f356b2e804881dd66fe4fea73e41c56cc92bc0b8
John Mettraux <jmettraux@gmail.com>
2019-02-26 17:37:33 +0900

    Introduce florist_rights table

--- lib/florist/migrations/0001_tables.rb
+++ lib/florist/migrations/0001_tables.rb
@@ -12,6 +12,9 @@ Sequel.migration do
   #                  +------+-----+
   #                  | assignment |
   #                  +------------+
+  # +-------+
+  # | right |
+  # +-------+
 
   up do
 
@@ -53,6 +56,9 @@ Sequel.migration do
 
       String :description, null: false
 
+      String :user    # who ordered the transition
+      String :domain  # level of authority for the transition[er]
+
       File :content # JSON
 
       String :ctime, null: false  # creation time
@@ -65,8 +71,8 @@ Sequel.migration do
 
       primary_key :id
 
-      #Integer task_id, null: false
-      Integer transition_id, null: false
+      #Integer :task_id, null: false
+      Integer :transition_id, null: false
 
       String :resource_type, null: false  # "user", "group", "role", ...
       String :resource_name, null: false  # "bob", "accounting"
@@ -75,14 +81,41 @@ Sequel.migration do
 
       String :description, null: false
 
-      String :ctime, null: false  # creation time
-      String :mtime, null: false  # last modification time
+      String :ctime, null: false  # creation time, Flor.tstamp
+      String :mtime, null: false  # last modification time, Flor.tstamp
 
       String :status, null: false
-        # 'active' or something else
+        # "active" or something else
 
       #index :task_id
       index :transition_id
+      index [ :resource_type, :resource_name ]
+    end
+
+    create_table :florist_rights do
+
+      primary_key :id
+
+      String :user, null: false  # "user123", "bob", "3456", ...
+
+      String :right, null: false
+        # "browse", "offer", "allocate", "start", "suspend"
+      String :statuses, null: false
+        # "created|offered", "allocated", ...
+
+      String :domain, null: false
+        # null ==> any domain
+
+      String :resource_type, null: true  # "user", "group", "role", ...
+      String :resource_name, null: true  # "bob", "accounting"
+        # null ==> any resource
+
+      String :ctime, null: false  # creation time, Flor.tstamp
+      String :mtime, null: false  # last modification time, Flor.tstamp
+
+      String :status, null: false
+        # "active" or something else
+
       index [ :resource_type, :resource_name ]
     end
   end
@@ -92,6 +125,8 @@ Sequel.migration do
     drop_table :florist_tasks
     drop_table :florist_transitions
     drop_table :florist_assignments
+
+    drop_table :florist_rights
   end
 end
 
