bc806b44ecd6071f92fc628662910f24e48831be
John Mettraux <jmettraux@gmail.com>
2019-04-28 11:30:35 +0900

    Move from list.rb to worklist.rb

--- lib/florist.rb
+++ lib/florist.rb
@@ -10,6 +10,6 @@ require 'flor/unit'
 require 'florist/migrations'
 require 'florist/errors'
 require 'florist/taskers'
-require 'florist/list'
 require 'florist/models'
+require 'florist/worklist'
 
