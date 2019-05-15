11cabfe1c69e49e10cd87a71bbcdc8039b3fd6f2
John Mettraux <jmettraux@gmail.com>
2019-05-15 17:18:07 +0900

    Reorganize worklist spec

--- spec/worklist_spec.rb
+++ spec/worklist_spec.rb
@@ -8,7 +8,7 @@
 require 'spec_helper'
 
 
-describe '::Florist' do
+describe '::Florist::Worklist' do
 
   before :each do
 
@@ -54,62 +54,60 @@ describe '::Florist' do
     @unit.shutdown
   end
 
-  describe '::Worklist' do
+  describe '.initialize' do
 
-    describe '.initialize' do
+    describe '(db, conf)' do
 
-      describe '(db, conf)' do
+      it 'instantiates a worklist' do
 
-        it 'instantiates a worklist' do
+        l = Florist::Worklist.new(@unit.storage.db)
 
-          l = Florist::Worklist.new(@unit.storage.db)
-
-          expect(l.task_ds.count).to eq(1)
-          expect(l.tasks.first.class.ancestors).to include(Sequel::Model)
-        end
+        expect(l.task_ds.count).to eq(1)
+        expect(l.tasks.first.class.ancestors).to include(Sequel::Model)
       end
+    end
 
-      describe '(unit)' do
+    describe '(unit)' do
 
-        it 'instantiates a worklist' do
+      it 'instantiates a worklist' do
 
-          l = Florist::Worklist.new(@unit)
+        l = Florist::Worklist.new(@unit)
 
-          expect(l.tasks.count).to eq(1)
-          expect(l.tasks.first.class.ancestors).to include(Sequel::Model)
-        end
+        expect(l.tasks.count).to eq(1)
+        expect(l.tasks.first.class.ancestors).to include(Sequel::Model)
       end
+    end
 
-      describe '(flor_db, florist_db)' do
+    describe '(flor_db, florist_db)' do
 
-        it 'instantiates a worklist' do
+      it 'instantiates a worklist' do
 
-          l = Florist::Worklist.new(@uri1, @uri2)
+        l = Florist::Worklist.new(@uri1, @uri2)
 
-          expect(l.flor_db.uri).to eq(@uri1)
-          expect(l.florist_db.uri).to eq(@uri2)
-          expect(l.florist_db.uri).not_to eq(l.flor_db.uri)
+        expect(l.flor_db.uri).to eq(@uri1)
+        expect(l.florist_db.uri).to eq(@uri2)
+        expect(l.florist_db.uri).not_to eq(l.flor_db.uri)
 
-          expect(l.tasks.count).to eq(1)
-          expect(l.tasks.first.tasker).to eq('bob')
-        end
+        expect(l.tasks.count).to eq(1)
+        expect(l.tasks.first.tasker).to eq('bob')
       end
+    end
 
-      describe '(no_florist_db)' do
+    describe '(no_florist_db)' do
 
-        it 'fails' do
+      it 'fails' do
 
-          expect {
-            Florist::Worklist.new()
-          }.to raise_error(
-            ArgumentError, "missing a florist database"
-          )
-        end
+        expect {
+          Florist::Worklist.new()
+        }.to raise_error(
+          ArgumentError, "missing a florist database"
+        )
       end
+    end
 
-      describe 'domain: "org.acme"' do
+    describe 'domain: "org.acme"' do
 
-        it 'instantiates a worklist limited to "org.acme.%"'# do
+      it 'instantiates a worklist limited to "org.acme.%"'# do
 
 # TODO bring me back
 #          l = Florist::Worklist.new(@unit, domain: 'org.acme')
@@ -123,16 +121,54 @@ describe '::Florist' do
 #          expect(l.tasks.count).to eq(1)
 #          expect(l.tasks.first.domain).to eq('org.acme.sub0')
 #        end
-      end
+    end
+  end
+end
+
+describe '::Florist::Worklist' do
+
+  before :each do
+
+    @unit = Flor::Unit.new(
+      loader: Flor::HashLoader,
+      sto_uri: storage_uri,
+      sto_migration_table: :flor_schema_info)
+    @unit.conf['unit'] = 'tskspec'
+    #@unit.hook('journal', Flor::Journal)
+    @unit.storage.delete_tables
+    @unit.storage.migrate
+    @unit.start
+
+    @unit.add_tasker('alice', Florist::WorklistTasker)
+    @unit.add_tasker('bob', Florist::WorklistTasker)
+
+    Florist.delete_tables(storage_uri)
+    Florist.migrate(storage_uri, table: :florist_schema_info)
+
+    @worklist = Florist::Worklist.new(@unit)
+  end
+
+  after :each do
+
+    @unit.shutdown
+  end
+
+  describe '#dump' do
+
+    before :each do
+
+      @unit.launch(%q{ alice _ }, domain: 'com.acme')
+      @unit.launch(%q{ alice _ }, domain: 'acme.org.test')
+      @unit.launch(%q{ bob _ }, domain: 'acme.org')
+
+      wait_until { @unit.executions.count == 3 }
     end
 
-    describe '#dump' do
+    describe '()' do
 
       it 'dumps' do
 
-        l = Florist::Worklist.new(@unit)
-
-        s = l.dump
+        s = @worklist.dump
 
         h = JSON.load(s)
 
@@ -142,8 +178,11 @@ describe '::Florist' do
           attls1 ctime data domain exid id mtime nid status tasker taskname ])
       end
     end
+  end
 
-    describe '#load' do
+  describe '#load' do
+
+    describe '()' do
 
       it 'loads'
     end
